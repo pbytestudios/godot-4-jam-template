80c1abf9fa39cdcaf3ece6df19c0b35c5c1de9ea
bncastle <bncastle@pixelbytestudios.com>
2023-09-27 13:42:45 -0400

    doodling with the SettingsData a bit

--- project/addons/pixelbyte/ui/settings/SettingsData.gd
+++ project/addons/pixelbyte/ui/settings/SettingsData.gd
@@ -13,54 +13,61 @@ const S_SFX :StringName= "sfxVol"
 const S_MUSIC :StringName= "musicVol"
 const S_AMBIENT :StringName= "ambientVol"
 const S_FULLSCREEN :StringName= "fullscreen"
-#if true, then the CRT shader is enabled
-const S_CRT_EFFECT :StringName= "use_crt_effect"
-
-#this holds any settings that are added via the add() function
-var other_settings: Array = []
 
 var cfg:ConfigFile
+
+# Defaults values for expected settings
+# 
+var defaults:Dictionary = {
+	S_MASTER : 100, S_AMBIENT : 100, S_MUSIC : 100, S_SFX : 100,
+	S_FULLSCREEN : false
+}
 
 func _init():
 	read()
 
 func _set(key:StringName, val:Variant) -> bool:
-	cfg.set_value(SETTINGS_SECTION, key, val)
+	#only set the value if there is no default for it
+	# or if it is different from the default
+	if cfg.has_section_key(SETTINGS_SECTION, key) or !defaults.has(key) or defaults[key] != val:
+		cfg.set_value(SETTINGS_SECTION, key, val)
 	return true
 
-func _get(key:StringName):
-	match key:
-		S_MASTER, S_SFX, S_MUSIC, S_AMBIENT: return cfg.get_value(SETTINGS_SECTION, key, 100)
-		S_FULLSCREEN: return cfg.get_value(SETTINGS_SECTION, key, false)
-		S_CRT_EFFECT: return cfg.get_value(SETTINGS_SECTION, key, false)
-		#Add any more settings default values here
-		_: 
-			if other_settings.has(key): 
-				return cfg.get_value(SETTINGS_SECTION, key)
-			printerr("'%s' is not a valid setting!" % key)
-			return 0
+# this allows me to add default settings values so that if a value is 
+# not present in the cfg file, these will be returned instead
+func add_default(key:StringName, default:Variant):
+	defaults[key] = default
 
-func add(key:String, val, section:String="Settings"):
-	if cfg.has_section_key(section, key): return
-	cfg.set_value(section, key, val)
+func get_or_default(key:StringName):
+	if cfg.has_section_key(SETTINGS_SECTION, key):
+		return cfg.get_value(SETTINGS_SECTION, key)
+	elif defaults.has(key):
+		return defaults[key]
+	else:
+		return null
+
+func _get(key:StringName):
+	return get_or_default(key)
 	
 # Note: I'm not checking error codes ...
 func read(filename:String = SETTINGS_FILE):
 	cfg = ConfigFile.new()
-	if !FileAccess.file_exists(filename):
-		cfg.save(filename)
-	else:
+	if FileAccess.file_exists(filename):
 		cfg.load(filename)
+	else: 
+		for k in defaults.keys():
+			cfg.set_value(SETTINGS_SECTION, k, defaults[k])
+		cfg.save(filename)
 
 # Note: I'm not checking error codes ...
 func save(filename:String = SETTINGS_FILE):
 	if cfg == null:
 		printerr("Config is not loaded!")
-	else:
+	elif cfg.has_section(SETTINGS_SECTION):
 		cfg.save(filename)
 
 static func set_vol(bus_name:String, value_db:int):
 	var index = AudioServer.get_bus_index(bus_name)
 	if index > -1:
 		AudioServer.set_bus_volume_db(index, value_db)
-		AudioServer.set_bus_mute(index, value_db == MIN_DB_VOL)		
+		AudioServer.set_bus_mute(index, value_db <= MIN_DB_VOL)		
