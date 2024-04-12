0ef35645e187df1e271b1e4d3e87357a4c53c131
bncastle <bncastle@pixelbytestudios.com>
2024-04-11 21:15:44 -0400

    More ui_effects work

--- project/addons/pixelbyte/ui/ui_effects.gd
+++ project/addons/pixelbyte/ui/ui_effects.gd
@@ -4,28 +4,37 @@ extends Node2D
 enum Mode {None, ToMarkOnReady, FromMarkOnReady, FadeIn, FadeOut}
 
 @export var parent:Control
+
 @export var tween_time:float = 1.0
 @export var tween_mode:Mode = Mode.None
 @export var play_ease:Tween.EaseType = Tween.EASE_IN
 @export var reverse_ease:Tween.EaseType = Tween.EASE_OUT
 @export var tween_trans: Tween.TransitionType = Tween.TRANS_LINEAR
 @export var play_tween:bool:
-	get: return play_tween
 	set(val):
-		if !Engine.is_editor_hint() || (is_instance_valid(_tw) && _tw.is_running()):
+		if _finishing || !Engine.is_editor_hint() || (is_instance_valid(_tw) && _tw.is_running()):
 			return
-		play_tween = false
 		_update_play_mode()
 		_save_state()
-		finished.connect(_restore_state, CONNECT_ONE_SHOT)
+		_finishing = true
+		finished.connect(func():
+			await get_tree().create_timer(0.125).timeout
+			_restore_state()
+			_finishing = false
+			, CONNECT_ONE_SHOT)
 		play()
-
-# func _validate_property(property: Dictionary):
-# 	if property.name == "play_tween" && (!Engine.is_editor_hint() || (is_instance_valid(_tw) && _tw.is_running())):
-# 		property.usage = PROPERTY_USAGE_NO_EDITOR
 		
-var _start_pos:Vector2
-var _mark_pos:Vector2
+var _start_pos:Vector2:
+	get: return $Start.global_position
+
+#func _validate_property(property: Dictionary) -> void:
+	#if property.name == "_start_pos":
+		#property.usage = PROPERTY_USAGE_NO_EDITOR
+
+var _mark_pos:Vector2:
+	get: 
+		return $Mark.global_position
+	
 var _tw:Tween
 
 var _play:Callable
@@ -35,17 +44,20 @@ var _reverse:Callable
 signal finished
 
 func _ready() -> void:
-	_start_pos = parent.global_position
-	_mark_pos = $Mark.global_position
+	set_process(Engine.is_editor_hint())
 	
-	if Engine.is_editor_hint():
-		return
-	
-	_update_play_mode()
-	if tween_mode == Mode.FromMarkOnReady || tween_mode == Mode.ToMarkOnReady:
-		print("o")
-		play()
+	if !Engine.is_editor_hint():
+		_update_play_mode()
+		if tween_mode == Mode.FromMarkOnReady || tween_mode == Mode.ToMarkOnReady:
+			play()
 
+func _process(delta: float) -> void:
+	_update_positions()
+
+func _update_positions():
+	if parent != null:
+		$Start.global_position = parent.global_position
+	
 func _update_play_mode():
 	match tween_mode:
 		Mode.ToMarkOnReady:
@@ -80,6 +92,7 @@ func reverse(): _reverse.call()
 
 ##### Editor-specific stuff
 var _state:Dictionary 
+var _finishing:bool
 func _save_state():
 	_state = {
 		"global_position" = parent.global_position,
