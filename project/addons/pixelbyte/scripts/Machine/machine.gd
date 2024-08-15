49e06ba99d9fc287359d5eafa58470fb4fdbc19b
bncastle <bncastle@pixelbytestudios.com>
2024-08-15 12:53:23 -0400

    Update machine

--- project/addons/pixelbyte/scripts/Machine/machine.gd
+++ project/addons/pixelbyte/scripts/Machine/machine.gd
@@ -1,12 +1,12 @@
 extends RefCounted
 class_name Machine
 
-# 2023 Pixelbyte Studios
+# Suffixes considered valid state suffixes
+const VALID_METHOD_SUFFIXES :Array[StringName] = ["_enter", "_update", "_exit"]
+
+# 2024 Pixelbyte Studios
 # a state machine using Callables
 # this allows all states ot be contained within 1 script
-
-# this holds all the states and their functions
-var state_map = {}
 
 var current_state_name:StringName:
 	get: 
@@ -23,7 +23,11 @@ var stopped:bool = true:
 var changing_states:bool:
 	get: return changing_states
 
+# this holds all the states and their functions
+var _state_map = {}
+# holds the previous state functions
 var _previous_state_functions:Dictionary = {}
+# holds the current state functions
 var _current_state_functions:Dictionary = {}
 
 var current_update:Callable
@@ -32,39 +36,52 @@ var current_update:Callable
 signal changed_state
 
 # function naming: the 'enter' callable will be used to name the state
+# if there is no enter function, then update, then exit
 func add(enter:Callable, update:Callable = Callable(), exit:Callable = Callable()):
-	var methods = {}
-	var state_name:String = "n/a"
+	var state_name:String = ""
 	
+	# Get the name of the state by trying the enter function 1st, then the other twqo
 	if enter.is_valid():
-		var slices:Array = enter.get_method().get_basename().trim_prefix("_").split('_', false, 2)
-	state_name = _get_state_name(enter)
-	if state_name.is_empty():
+		state_name = _get_state_name(enter)
+	if state_name.is_empty() && update.is_valid():
 		state_name = _get_state_name(update)
+	if state_name.is_empty() && exit.is_valid():
+		state_name = _get_state_name(exit)
 
-	methods["enter"] = enter
-	methods["update"] = update
-	methods["exit"] = exit
-	methods["name"] = state_name
-	state_map[state_name] = methods
+	#Create a 'map' of this state and its methods
+	var methods:Dictionary = {}
+	methods.enter = enter
+	methods.update = update
+	methods.exit = exit
+	methods.name = state_name
+	
+	if _state_map.has(state_name):
+		push_warning("[Machine] Warning: State name '%s' already in map!" % state_name)
+		
+	_state_map[state_name] = methods
 
-func _get_state_name(callable:Callable) -> String: 
-	if callable.is_valid():
-		return callable.get_method().get_basename().trim_prefix("_").trim_suffix("_enter").trim_suffix("_update").trim_suffix("_exit")
-	return ""	
-#
-# Stops the state machine
-# Note: the currently-running state's exit function will be called if it exists
-#
+func remove(state_func:Callable):
+	var state_name:String = ""
+	state_name = _get_state_name(state_func)
+	if state_name.is_empty():
+		push_warning("[Machine] Warning: Unable to get state name from %s" % state_func.get_method().get_basename())
+	elif !_state_map.has(state_name):
+		push_warning("[Machine] Warning: Map does not contain '%s'" % state_name)
+	else:
+		_state_map.erase(state_name)
+
+## Starts the state machine with the given state
+func start(starting:Callable):
+	stopped = false
+	change(starting)
+	
+## Stops the state machine
+## Note: the currently-running state's exit function will be called if it exists
+##
 func stop():
 	stopped = true
 	current_state_name = ""
 	_change_state({})
-
-func start(starting:Callable):
-	stopped = false
-	change(starting)
-
 
 func change_prev() -> bool:
 	if _previous_state_functions.is_empty():
@@ -82,11 +99,20 @@ func change(to:Callable):
 		return
 	
 	var state_name = _get_state_name(to)
-	if !state_map.has(state_name):
+	if !_state_map.has(state_name):
 		printerr("Cannot find state: %s!" % to)
 		return
 	
-	await _change_state(state_map[state_name])
+	await _change_state(_state_map[state_name])
+
+func _get_state_name(callable:Callable) -> String: 
+	var name:String = ""
+	if callable.is_valid():
+		name =  callable.get_method().get_basename().trim_prefix("_")
+		# trim all possible suffixes for a state function
+		for suffix in VALID_METHOD_SUFFIXES:
+			name = name.trim_suffix(suffix)
+	return name
 
 func _change_state(state_funcs:Dictionary):
 	if changing_states:
@@ -95,10 +121,10 @@ func _change_state(state_funcs:Dictionary):
 	changing_states = true
 	
 	#clear the update function, so we don't run it during the transition
-	current_update = Callable()
+	current_update = update_empty
 
-	if !_current_state_functions.is_empty() && _current_state_functions["exit"].is_valid():
-		_current_state_functions["exit"].call()
+	if !_current_state_functions.is_empty() && _current_state_functions.exit.is_valid():
+		_current_state_functions.exit.call()
 	
 	if state_funcs != _current_state_functions:
 		_previous_state_functions = _current_state_functions
@@ -108,16 +134,18 @@ func _change_state(state_funcs:Dictionary):
 	#print(state_funcs)
 	
 	#call the new state's enter if there is one
-	if !_current_state_functions.is_empty() && _current_state_functions["enter"].is_valid():
-		_current_state_functions["enter"].call()
+	if !_current_state_functions.is_empty() && _current_state_functions.enter.is_valid():
+		_current_state_functions.enter.call()
 
 	#setup the update and physics functions if they exist
-	if !_current_state_functions.is_empty() && _current_state_functions["update"].is_valid() && !stopped:
-		current_update = _current_state_functions["update"]
+	if !_current_state_functions.is_empty() && _current_state_functions.update.is_valid() && !stopped:
+		current_update = _current_state_functions.update
 
 	changing_states = false
 	changed_state.emit()
-	
+
+#an empty update function
+func update_empty(delta:float): pass
+
 func update(delta:float):
-	if current_update.is_valid():
-		current_update.call(delta)
+	current_update.call(delta)
