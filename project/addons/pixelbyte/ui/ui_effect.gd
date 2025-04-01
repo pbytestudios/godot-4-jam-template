f94d1c3ba8ee6aeb9f569ce2c112d1a930f47239
bncastle <bncastle@pixelbytestudios.com>
2025-03-31 21:04:57 -0400

    refacor ui_effect, fixed main menu

--- project/addons/pixelbyte/ui/ui_effect.gd
+++ project/addons/pixelbyte/ui/ui_effect.gd
@@ -3,48 +3,31 @@ class_name UIEffect
 extends Node2D
 
 enum Mode {None, ToMark, FromMark, FadeIn, FadeOut}
+enum State {Ready, Played}
 
 @export var target:Control
 @export var start:Marker2D
 @export var mark:Marker2D
+
 @export_category("Tween Properties")
 @export var play_on_ready:bool = false
-@export var tween_time:float = 1.0
+@export var tween_play_time:float = 1.0
+@export var tween_reverse_time:float = 0.5
 @export var tween_mode:Mode = Mode.None
 @export var play_ease:Tween.EaseType = Tween.EASE_IN
 @export var reverse_ease:Tween.EaseType = Tween.EASE_OUT
 @export var tween_trans: Tween.TransitionType = Tween.TRANS_LINEAR
+
 @export_category("Editor Controls")
-@export var set_mark:bool:
-	set(val):
-		if val && !_finishing && Engine.is_editor_hint() && is_instance_valid(target):
-			mark.global_position = target.global_position
-			
-@export var play_tween:bool:
-	set(val):
-		if _finishing || !Engine.is_editor_hint() || (is_instance_valid(_tw) && _tw.is_running()):
-			return
-			
-		_update_play_mode()
-		_save_state()
-		_finishing = true
-		finished_play.connect(func():
-			await get_tree().create_timer(0.125).timeout
-			_restore_state()
-			_finishing = false
-			, CONNECT_ONE_SHOT)
-		play()
-		
+
+var state:State = State.Ready:
+	get: return state
+
 var _start_pos:Vector2:
 	get: return start.global_position
 
-#func _validate_property(property: Dictionary) -> void:
-	#if property.name == "_start_pos":
-		#property.usage = PROPERTY_USAGE_NO_EDITOR
-
 var _mark_pos:Vector2:
-	get: 
-		return mark.global_position
+	get: return mark.global_position
 	
 var _tw:Tween
 
@@ -52,9 +35,7 @@ var _play:Callable
 var _reverse:Callable
 
 ## Emitted when the ui action finishes
-signal finished_play
-## Emitted when the ui action finishes reversing
-signal finished_reverse
+signal finished
 
 func _ready() -> void:
 	set_process(Engine.is_editor_hint())
@@ -75,23 +56,23 @@ func _update_positions():
 	
 func _update_play_mode():
 	match tween_mode:
-		Mode.None:
-			_play = Callable()
-			_reverse = Callable()
 		Mode.ToMark:
-			_play = func(): _do_tween("global_position", play_ease, _mark_pos, tween_time)
-			_reverse = func(): _do_tween("global_position",reverse_ease, _start_pos, tween_time, true)
+			_play = func(): _do_tween("global_position", play_ease, _mark_pos, tween_play_time)
+			_reverse = func(): _do_tween("global_position",reverse_ease, _start_pos, tween_reverse_time, true)
 		Mode.FromMark:
 			_play = func():
 				target.global_position = _mark_pos
-				_do_tween("global_position", play_ease, _start_pos, tween_time, true)
-			_reverse = func(): _do_tween("global_position", reverse_ease, _mark_pos, tween_time)
+				_do_tween("global_position", play_ease, _start_pos, tween_play_time, true)
+			_reverse = func(): _do_tween("global_position", reverse_ease, _mark_pos, tween_reverse_time)
 		Mode.FadeIn:
-			_play = func():_fade(play_ease, 0.0, 1.0, tween_time)
-			_reverse = func():_fade(reverse_ease, 1.0, 0.0, tween_time, true)
+			_play = func():_fade(play_ease, 0.0, 1.0, tween_play_time)
+			_reverse = func():_fade(reverse_ease, 1.0, 0.0, tween_reverse_time, true)
 		Mode.FadeOut:
-			_play = func():_fade(play_ease, 1.0, 0.0, tween_time)
-			_reverse = func():_fade(reverse_ease, 0.0, 1.0, tween_time, true)
+			_play = func():_fade(play_ease, 1.0, 0.0, tween_play_time)
+			_reverse = func():_fade(reverse_ease, 0.0, 1.0, tween_reverse_time, true)
+		Mode.None, _:
+			_play = Callable()
+			_reverse = Callable()
 
 func _do_tween(property:String, ease:Tween.EaseType, value, time:float, reverse:bool = false):
 	if is_instance_valid(_tw):
@@ -99,10 +80,14 @@ func _do_tween(property:String, ease:Tween.EaseType, value, time:float, reverse:
 
 	_tw = create_tween().set_ease(ease).set_trans(tween_trans)
 	_tw.tween_property(target, property, value, time)
-	if reverse:
-		_tw.tween_callback(func(): finished_reverse.emit())
+	_tw.tween_callback(_on_tween_finished)
+
+func _on_tween_finished():
+	if state == State.Ready:
+		state = State.Played
 	else:
-		_tw.tween_callback(func(): finished_play.emit())
+		state = State.Ready
+	finished.emit()
 
 func is_playing(): return is_instance_valid(_tw) && _tw.is_running()
 	
@@ -110,10 +95,43 @@ func _fade(ease:Tween.EaseType, start_alpha:float, stop_alpha:float, time:float,
 	target.modulate.a = start_alpha
 	_do_tween("modulate:a", ease, stop_alpha, time, reverse)
 
-func play(): _play.call()
-func reverse(): _reverse.call()
+func play(): 
+	if _play.is_valid():
+		_play.call()
+	else:
+		_on_tween_finished()
+	return self
+
+func reverse(): 
+	if _reverse.is_valid():
+		_reverse.call()
+	else:
+		_on_tween_finished()
+	return self
 
 ##### Editor-specific stuff
+# for editor icon string names, visit: https://godot-editor-icons.github.io/
+@export_tool_button("Set Mark", "Anchor") var mark_action = set_mark
+@export_tool_button("Play Tween", "Play") var play_action = play_tween
+
+func set_mark():
+	if !_finishing && Engine.is_editor_hint() && is_instance_valid(target):
+		mark.global_position = target.global_position
+
+func play_tween():
+	if _finishing || !Engine.is_editor_hint() || (is_instance_valid(_tw) && _tw.is_running()):
+		return
+		
+	_update_play_mode()
+	_save_state()
+	_finishing = true
+	finished.connect(func():
+		await get_tree().create_timer(0.125).timeout
+		_restore_state()
+		_finishing = false
+		, CONNECT_ONE_SHOT)
+	play()
+	
 var _state:Dictionary 
 var _finishing:bool
 func _save_state():
--- project/screens/main_menu.gd
+++ project/screens/main_menu.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var game_scene:PackedScene
 @export var title_label: Label
+@export var menu_effects:UIEffect
 
 func _ready():
 	if !Engine.is_editor_hint():
@@ -9,9 +10,9 @@ func _ready():
 		Wiper.wipe_immediate()
 		await Wiper.unwipe()
 		
-	$Ui/ControlEffects/MainMenu.play()
+	await menu_effects.play().finished
 	
-	if OS.get_name() == "HTML5":
+	if OS.get_name() == "Web":
 		$Ui/PanelContainer/MarginContainer/VBoxContainer/Exit.visible = false
 
 func _notification(what: int) -> void:
@@ -20,26 +21,26 @@ func _notification(what: int) -> void:
 			title_label.text = ProjectSettings.get_setting("application/config/name")
 			
 func disable_buttons():
-	for element in $Ui/MainMenuContainer/MarginContainer/VBoxContainer.get_children():
+	for element in $Ui/MainMenu/MarginContainer/VBoxContainer.get_children():
 		if element is Button:
 			element.disabled = true
 	
 func _on_play():
 	if game_scene != null:
 		disable_buttons()
-		await Wiper.wipe()
+		await menu_effects.reverse().finished
 		Wiper.wipe_to_packed(game_scene)
 	else:
 		printerr("game_scene must be specified!")
 
 func _on_settings():
-	$Ui/MainMenuContainer.visible = false
+	$Ui/MainMenu.visible = false
 	$Ui/SettingsPanel.show_dlg()
 	await $Ui/SettingsPanel.hidden
-	$Ui/MainMenuContainer.visible = true
+	$Ui/MainMenu.visible = true
 
 func _on_exit():
 	disable_buttons()
-	await get_tree().create_timer(0.25).timeout
+	await menu_effects.reverse().finished
 	await Wiper.wipe()
 	get_tree().quit()
