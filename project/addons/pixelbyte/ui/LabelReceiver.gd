0822e74a4a8e833f7e0248362518a87feb707c00
bncastle <bncastle@pixelbytestudios.com>
2025-04-12 18:21:09 -0400

    - LabelReceiver can now fade text

--- project/addons/pixelbyte/ui/LabelReceiver.gd
+++ project/addons/pixelbyte/ui/LabelReceiver.gd
@@ -5,6 +5,9 @@ enum LabelReadyState {None, ClearOnReady, PrefixOnReady}
 @export var prefix:String = ""
 @export var suffix:String = ""
 
+@export var fade_time:float = 0.0
+@export var wait_to_fade_time:float = 0.0
+
 @export var label_ready_state:LabelReadyState = LabelReadyState.None
 
 func _ready() -> void:
@@ -13,13 +16,36 @@ func _ready() -> void:
 			text = ""
 		LabelReadyState.PrefixOnReady:
 			text = prefix
-
+	# make sure there is SOME fade time if there is wait_fade time
+	fade_time = max(0.25, fade_time)
 
 func _receive_int(val:int):
 	text = "%s%d%s" % [prefix, val, suffix]
+	start_fade()
 	
 func _receive_float(val:float):
 	text = "%s%.2f%s" % [prefix, val, suffix]
+	start_fade()
 
 func _receive_str(val:String):
 	text = "%s%s%s" % [prefix, val, suffix]
+	start_fade()
+
+func start_fade():
+	if wait_to_fade_time > 0:
+		if modulate.a < 1.0:
+			fade(1.0, .25).tween_callback(func(): fade(0.0, fade_time, wait_to_fade_time))
+		else:
+			fade(0.0, fade_time, wait_to_fade_time)
+
+var tw:Tween
+func fade(to:float, time:float = 0.25, time_before_start:float = 0.0) -> Tween:
+	if tw && tw.is_running():
+		tw.kill()
+	tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+		
+	if time_before_start > 0:
+		tw.tween_interval(time_before_start)
+	tw.tween_property(self, "modulate:a", to, time * abs(to - modulate.a))
+
+	return tw
