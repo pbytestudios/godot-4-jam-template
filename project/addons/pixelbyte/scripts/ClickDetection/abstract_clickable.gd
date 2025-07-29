3ddde7aa30c233075eafa3ea1f9b9104189ed0ae
bncastle <bncastle@pixelbytestudios.com>
2025-07-29 08:34:39 -0400

    reamed up() signal to unclicked()

--- project/addons/pixelbyte/scripts/ClickDetection/abstract_clickable.gd
+++ project/addons/pixelbyte/scripts/ClickDetection/abstract_clickable.gd
@@ -1,9 +1,13 @@
 @abstract class_name AbstractClickable
 extends Node
 
+## Emitted when the mouse is clicked
 signal clicked(button:MouseButton)
+## Emitted when the mouse is "unclicked"
+signal unclicked(button:MouseButton)
+## Emitted when the mouse is double clicked
 signal double_clicked(button:MouseButton)
-signal up(button:MouseButton)
+## Emitted when the mouse starts or stops hovering over the object
 signal hovering(is_hovering:bool)
 
 func _on_mouse_exited(): hovering.emit(false)
@@ -14,10 +18,11 @@ func _process_input_events(event: InputEvent) -> void:
 		var mbe:InputEventMouseButton = event
 		if mbe.pressed:
 			if mbe.double_click:
-				print("double click")
+				#print("double click")
 				double_clicked.emit(mbe.button_index)
 			else:
-				print("click")
+				#print("click")
 				clicked.emit(mbe.button_index)
 		elif mbe.is_released():
-			up.emit(mbe.button_index)
+			#print("up")
+			unclicked.emit(mbe.button_index)
