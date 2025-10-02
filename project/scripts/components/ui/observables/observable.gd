e4024fd3bd0208101adde3f8b0e284a893161777
bncastle <bncastle@pixelbytestudios.com>
2025-10-02 17:48:45 -0400

    fixed signal subscription bug

--- project/scripts/components/ui/observables/observable.gd
+++ project/scripts/components/ui/observables/observable.gd
@@ -4,16 +4,16 @@ extends Resource
 # modified from 
 # https://github.com/NoBS-Code/godot-reactive-ui
 
-signal observable_changed(observable:Observable)
+signal updated(observable:Observable)
 
 var owner: Observable:
 	set(val):
 		if owner != null:
-			owner.observable_changed.disconnect(owner._propogate)
+			updated.disconnect(owner._propagate)
 		owner = val
 		if owner != null:
-			owner.observable_changed.connect(owner._propagate)
+			updated.connect(owner._propagate)
 
 func _init(_owner:Observable = null) -> void: owner = _owner
-func _propagate(observable:Observable = null) -> void: observable_changed.emit(self)
-func emit_manual() -> void: _propagate()
+func _propagate(observable:Observable = null) -> void: updated.emit(self)
+func emit_manual() -> void: updated.emit(self)
